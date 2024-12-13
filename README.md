a44c3492913b9a38b37b7cabb3a1e6644537ae53
howmp <zhaopeiyuan6@gmail.com>
2024-12-13 18:55:22 +0800

    opt size

--- README.md
+++ README.md
@@ -6,10 +6,29 @@
 
 wget.exe https://www.qq.com 1.html
 
+### 缓存问题
+
+添加一个随机参数
+
+wget.exe https://www.qq.com/?ts=11111 1.html
+
 ## 下载
 
-<https://github.com/howmp/wget/blob/main/wget.exe>
+<https://github.com/howmp/wget/blob/main/wgetmini.exe>
 
 ## 编译
 
-需要用VC6编译
+1. 用VC6编译
+1. 删除文件尾部\x00
+1. patch
+   1. PE的DOS_STUB,将两个字符串常量覆盖
+   1. 根据偏移修改printf附近的汇编代码指向常量
+   
+
+## 更新
+
+### 2024年12月13日
+
+根据大佬[Laster-dev](https://github.com/Laster-dev)的指点，优化大小
+
+1024 -> 862
--- wget.c
+++ wget.c
@@ -8,30 +8,34 @@
 #pragma comment(linker, "/OPT:NOWIN98")
 #pragma comment(linker, "/merge:.data=.text")
 #pragma comment(linker, "/merge:.rdata=.text")
+int __getmainargs(int *, char ***, char ***, int, void *);
 
 void main()
 {
 	int argc = 0;
-	DWORD exitcode = 0;
-	int run = 0;
-	LPWSTR* argvw = CommandLineToArgvW(GetCommandLineW(),&argc);
+	int error = 0;
+	char** argv = NULL;
+	char** envp = NULL;
+	__getmainargs(&argc,&argv,&envp,0,NULL);
+	
 	if(argc<3){
-		printf("usage: wget url filepath\n");
-		exitcode = 1;
-	}else{
-		int error = URLDownloadToFileW(NULL, argvw[1], argvw[2], 0, NULL);
-		if (error != S_OK){
-			printf("download error 0x%08x",error);
-			exitcode = 2;
-		}else if(argc==4){	
-			
+		// usage: wget url filepath
+		printf(0x400000);
+		exit(1);
+	}
+	error = URLDownloadToFileA(NULL, argv[1], argv[2], 0, NULL);
+	if (error != S_OK){
+		// download error 0x%08x
+		printf(0x400000,error);
+		exit(2);
+	}
+	//if(argc==4){	
 			//error = (int)ShellExecuteW(NULL,NULL,argvw[2],NULL,NULL,0);
 			//if(error > 32){
 			//	printf("run error 0x%08x",error);
 			//	exitcode = 3;
 			//}
-		}
-	}
-	ExitProcess(exitcode);
+	//}
+	exit(0);
 }
 
